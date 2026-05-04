5e6ce7a4187e87c303be9b3ce1a5a634af465b8b
prabhatxmishra <prabhatmishra2402@gmail.com>
2026-05-04 22:31:16 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/telecom/phonenumber/PhonenumberApplication.java
@@ -0,0 +1,13 @@
+package com.telecom.phonenumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonenumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonenumberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telecom/phonenumber/PhonenumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telecom.phonenumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonenumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
